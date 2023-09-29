fb1fbaf7dd16bd0e3e0c0d1427a892f285db4461
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-09-29 16:22:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _2._ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = int.Parse(Console.ReadLine());
+            int with = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+
+            if (with > limit)
+                Console.WriteLine("The limit was exceeded.");
+
+            else if (with > balance)
+                Console.WriteLine("Insufficient availability.");
+
+            else
+                Console.WriteLine("The withdraw was successful.");
+        }
+    }
+}
